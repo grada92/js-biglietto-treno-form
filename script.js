3365a776e69e966ee0d6d433882cb12ffa386889
grada92 <danielegradassai@gmail.com>
2022-08-24 20:15:28 +0200

    inserisco offerta

--- script.js
+++ script.js
@@ -18,33 +18,37 @@ result.addEventListener('click',
         let addKm = parseInt(km.value);
         console.log(addKm);
 
-
-
-
         // Calcolo Biglietto
         
         let biglietto = addKm * 0.21;
-        
-        
+        let offerta = 'biglietto standard';
+
+
+               
 
     if (addAge < 18) {
     let Percentuale20 = (biglietto / 100) * 20;
     biglietto = biglietto - Percentuale20;
-    
+    offerta = 'biglietto scontato';
        
+      
     } else if (addAge > 65) {
     let Percentuale40 = (biglietto / 100) * 40;
     biglietto = biglietto - Percentuale40;
+    offerta = 'biglietto scontato';
     
-
 
     }
     
     document.getElementById('ticket').innerHTML = biglietto.toFixed(2) + '€' ;
     document.getElementById('namepassenger').innerHTML = addUser;
+    document.getElementById('gift').innerHTML = offerta ;
+
     }
 
 )
+
+
 
 
 
